4d404437fb3cbd5389191f23802e755cc9e42416
Supanat Wanroj <supanat.wanroj@scbtechx.io>
2022-03-02 17:34:48 +0700

    Add list of containner

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,38 @@ class _MyHomePageState extends State<MyHomePage> {
                 placeholder: "SEARCH ANY INVESTMENT PRODUCTS YOU WANT",
                 style: TextStyle(fontSize: 14.0),
               ),
-            )
+            ),
+            const Text("My watch list", textAlign: TextAlign.start),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20.0),
+              height: 200.0,
+              child: ListView(
+                // This next line does the trick.
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 160.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
