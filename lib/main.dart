e3153b349b51c3ed50e04f382daae9148b7065f3
Supanat Wanroj <supanat.wanroj@scbtechx.io>
2022-03-11 17:17:43 +0700

    Add expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -30,14 +30,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Container cardListView() {
+  Container cardListView(int itemCount) {
     return Container(
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
         height: 200.0,
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
-          itemCount: 3,
+          itemCount: itemCount,
           itemBuilder: (context, index) {
             return Container(
               width: MediaQuery.of(context).size.width * 0.5,
@@ -52,7 +52,20 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Column(children: [
                       Image.network(
                           "https://mildspace.files.wordpress.com/2021/12/giveban.png?w=1680"),
-                      Text("test1")
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text("test1"),
+                            flex: 4,
+                          ),
+                          Expanded(
+                            child: IconButton(
+                                onPressed: () => print("fav"),
+                                icon: const Icon(Icons.favorite)),
+                            flex: 1,
+                          )
+                        ],
+                      )
                     ]),
                   ),
                 ),
@@ -91,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   child:
                       Text("My watch list", style: TextStyle(fontSize: 18.0)),
                 )),
-            cardListView(),
+            cardListView(3),
           ],
         ),
       ),
